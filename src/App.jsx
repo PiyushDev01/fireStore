ea1f9ae6f7d296a814d6fb226cc69e2c6c1d02de
PiyushDev01 <piyushvishwakarma6707@gmail.com>
2024-07-19 00:02:40 +0530

    still updating

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Form from './Form'
 import DetailCard from './DetailCard'
 import Login from './Login'
 import ProtectedRoutes from './ProtectedRoutes'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
  
 
@@ -14,7 +15,7 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Login />} />
-      
+      {/* protected routing */}
       <Route element={<ProtectedRoutes/>} >
         <Route path='form' element={<Form />} />
         <Route path='detail' element={<DetailCard />} />
